14c31c5ef1d7d75428a4f742d2f4c2baf65f6ace
夏昸 <BH5UOE@126.com>
2026-04-03 23:03:39 +0800

    fix: optimize config and build scripts

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# diy-part1.sh - 在 update feeds 之前执行
+# 用途：修改 feeds.conf.default，添加第三方插件源
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 添加 SSR-Plus (helloworld) 源
+#    LEDE 的 feeds.conf.default 中没有预置此行，需要直接追加
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 2. 添加 OpenClash 源
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >> feeds.conf.default
+
+# 3. 添加 Argon 主题源（确保获取最新版）
+echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon.git;master' >> feeds.conf.default
+
+# 4. 添加 AdGuard Home 源
+echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git' >> feeds.conf.default
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# diy-part2.sh - 在 install feeds 之后执行
+# 用途：修改默认配置，预置自定义设置
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改默认 LAN IP（避免与常见家用路由器冲突）
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 修改默认主机名
+sed -i 's/OpenWrt/MyOpenWrt/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 启用 BBR TCP 拥塞控制 + FQ 队列调度（x86 内核 >= 4.9 均支持）
+mkdir -p package/base-files/files/etc/sysctl.d
+cat > package/base-files/files/etc/sysctl.d/10-bbr.conf << 'EOF'
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
+
+# 4. 提高系统连接数上限（适合高负载 x86 路由场景）
+cat > package/base-files/files/etc/sysctl.d/11-conntrack.conf << 'EOF'
+net.netfilter.nf_conntrack_max=131072
+net.nf_conntrack_max=131072
+EOF
+
+# 5. 设置 LuCI 默认主题为 Argon
+uci_file="package/base-files/files/etc/uci-defaults/99-set-argon-theme"
+mkdir -p "$(dirname "$uci_file")"
+cat > "$uci_file" << 'EOF'
+#!/bin/sh
+uci set luci.main.mediaurlbase='/luci-static/argon'
+uci commit luci
+exit 0
+EOF
+chmod +x "$uci_file"
