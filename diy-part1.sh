24504c02155ebeda7ae76e3d20b2efeeaf0802f1
夏昸 <BH5UOE@126.com>
2026-04-05 23:21:06 +0800

    fix: 补全微信推送与 WireGuard 插件，清理 ESXi 磁盘配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,7 +7,11 @@
 # 1. 添加 OpenClash 源 (作为主力科学上网插件)
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >> feeds.conf.default
 
-# 2. 添加 全能推送 (PushBot/微信推送)
-echo 'src-git pushbot https://github.com/zzsj0928/luci-app-pushbot' >> feeds.conf.default
+# 2. 添加 helloworld 源 (作为备用依赖源)
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+
+# 3. 添加 全能推送 (PushBot/微信推送)
+# 改为手动 git clone，防止被 make defconfig 静默删除
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 
 # 注：luci-theme-argon 和 luci-app-adguardhome 已内置于 LEDE 官方 feeds，无需单独添加
