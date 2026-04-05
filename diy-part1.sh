95358ea9643c52e1ec1d5dbb061793b4dccdec6f
夏昸 <BH5UOE@126.com>
2026-04-05 11:27:49 +0800

    Feature: add luci-app-pushbot (全能推送)

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,4 +7,7 @@
 # 1. 添加 OpenClash 源 (作为主力科学上网插件)
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >> feeds.conf.default
 
+# 2. 添加 全能推送 (PushBot/微信推送)
+echo 'src-git pushbot https://github.com/zzsj0928/luci-app-pushbot' >> feeds.conf.default
+
 # 注：luci-theme-argon 和 luci-app-adguardhome 已内置于 LEDE 官方 feeds，无需单独添加
