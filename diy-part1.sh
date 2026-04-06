c639513d621cf7ff40e1106b08c37c53aed54968
夏昸 <BH5UOE@126.com>
2026-04-07 00:05:26 +0800

    修复 DDNS 插件问题：添加独立版 AliDDNS 下载源并显式屏蔽系统默认 DDNS

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,10 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >> feeds
 rm -rf package/luci-app-pushbot
 git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 
+# 3. 添加 阿里云盘 DDNS (luci-app-aliddns) 独立插件
+rm -rf package/luci-app-aliddns
+git clone https://github.com/honwen/luci-app-aliddns.git package/luci-app-aliddns
+
 # 注：luci-theme-argon 和 luci-app-adguardhome 已内置于 LEDE 官方 feeds，无需单独添加
 
 # 3. 添加 Argon 主题设置插件 (luci-app-argon-config)
