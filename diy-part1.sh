dd2e0c2c06fd27d2e7d8f87faabe3148b8e41ab6
夏昸 <BH5UOE@126.com>
2026-04-06 15:00:53 +0800

    修复阿里云DDNS配置错误并引入独立版本，追加Argon主题设置

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,3 +12,6 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >> feeds
 git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 
 # 注：luci-theme-argon 和 luci-app-adguardhome 已内置于 LEDE 官方 feeds，无需单独添加
+
+# 3. 添加 Argon 主题设置插件 (luci-app-argon-config)
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
