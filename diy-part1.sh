17339206258b023d3a9f3947b6af475dd6e8f313
夏昸 <BH5UOE@126.com>
2026-04-16 00:00:24 +0800

    移除 OpenAppFilter 插件相关配置及 README 说明

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,14 +16,5 @@ rm -rf package/luci-app-pushbot
 rm -rf package/luci-app-wechatpush
 git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
 
-# 3. 阿里云 DDNS (luci-app-aliddns)
-# 由于上游长期未更新，已将源码固化在本地 packages 目录下，在 diy-part2.sh 中处理
 
-# 注：luci-theme-argon 和 luci-app-adguardhome 已内置于 LEDE 官方 feeds，无需单独添加
 
-# 4. Argon 主题设置插件 (luci-app-argon-config)
-# 由于上游长期未更新，已将源码固化在本地 packages 目录下，在 diy-part2.sh 中处理
-
-# 4. 添加 OpenAppFilter (应用过滤)
-rm -rf package/OpenAppFilter
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
